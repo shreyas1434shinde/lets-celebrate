5ca7443710160c9c225e6817cf35f238ab7ce715
TTPL-RT-52 <shreyas1434shinde@gmail.com>
2023-05-07 22:17:33 +0530

    all perfect chagnes

--- js/app.js
+++ js/app.js
@@ -22,20 +22,25 @@ fetch('../asset/data.json')
 .then(res => res.json())
 .then(data => {
     data.forEach(post => {
-        ListE.insertAdjacentHTML('beforeend', `<div class="col-md-4 none-3">
+        ListE.insertAdjacentHTML('beforeend', `<div class="none-3 col-md-4">
         <div class="card12 mb-3">
           <div class="content1">
-            <div class="front">
-              <div>${post.title}</div>
-              <div class="pb-5 mb-5">${post.subTitle}</div>
+            <div class="front px-3">
+              <div class="core mt-5"> ${post.core} </div>
+              <div class="card-title mt-3"> ${post.title}</div>
+              <div class=" mt-3 mb-5 card-sub-title"> ${post.subTitle}</div>
+              <div class="pt-2">
+                <div class="text-white"> ${post.early}</div>
+                <img src="" alt="">
+              </div>
             </div>
-            <div class="back">
-             <div>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, reprehenderit cumque soluta saepe nulla inventore eos quidem laudantium beatae. Rerum,
-             </div>
-             <div>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem iure excepturi, impedit nemo 
-             </div>
+            <div class="back px-3">
+              <div class="core1 mt-5"> ${post.core} </div>
+              <div class="pt-3">If they were to follow their natural instincts, the family unit has everything that it takes to bring up a child</div>
+              <div class="pt-3">
+                The system makes the family, and through it, the child feels inadequate. A reassurance is all that their wounded self-esteem needs.
+              </div>
+              <div class="text-blue pt-5 mt-5">EARLY CHILDHOOD</div>
             </div>
           </div>
         </div>
@@ -44,7 +49,7 @@ fetch('../asset/data.json')
       
     });
     $(function () {
-      $(".none-3").slice(0, 18).show();
+      $(".none-3").slice(0, 6).show();
       $("body").on('click touchstart', '.load-more', function (e) {
           e.preventDefault();
           $(".none-3:hidden").slice(0, 3).slideDown();
